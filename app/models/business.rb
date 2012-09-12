6e4bdc1ecbcc1ded5bdd305c14cbad8e2987bf1e
Cesar G <me@mrebola.com>
2012-09-12 12:59:03 -0500

    search added

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,15 @@
 class Business < ActiveRecord::Base
-  attr_accessible :address, :cellphone, :city, :description, :email, :keywords, :name, :phone, :website
+attr_accessible :address, :cellphone, :city, :description, :email, :keywords, :name, :phone, :website
+
+	def self.search(search)
+		if search
+			find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+		else
+			#find(:all)
+		end
+	end
+
 end
+
+
+
